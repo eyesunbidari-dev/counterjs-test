fced5756bd16e2d81b260116792a8bc856617a12
eyesun <eyesunbidari@gmail.com>
2024-01-07 19:11:49 +0330

    second approach for btns add

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
-const incrementBtn = document.querySelector(".increment");
+// const incrementBtn = document.querySelector(".increment");
+// const counterValue = document.querySelector(".counter__value");
+// const resetBtn = document.querySelector(".reset");
+// let count = Number(counterValue.textContent);
+// incrementBtn.addEventListener("click", () => {
+//    count++;
+//    counterValue.textContent = count;
+// });
+// const decrementBtn = document.querySelector(".decrement");
+// decrementBtn.addEventListener("click", () => {
+//    count--;
+//    counterValue.textContent = count;
+// })
+// function reset () {
+//    count = 0;
+//    counterValue.textContent=count
+// }
+// resetBtn.addEventListener("click" , reset);
+
+// second approach
 const counterValue = document.querySelector(".counter__value");
-const resetBtn = document.querySelector(".reset");
 let count = Number(counterValue.textContent);
-incrementBtn.addEventListener("click", () => {
-   count++;
-   counterValue.textContent = count;
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const classList = btn.classList;
+
+    if (classList.contains("increment")) count++;
+    else if (classList.contains("decrement")) count--;
+    else count = 0;
+
+    if (count > 0) counterValue.style.color = "green";
+    else counterValue.style.color = "red";
+
+    counterValue.textContent = count;
+  });
 });
-const decrementBtn = document.querySelector(".decrement");
-decrementBtn.addEventListener("click", () => {
-   count--;
-   counterValue.textContent = count;
-})
-function reset () {
-   count = 0;
-   counterValue.textContent=count
-}
-resetBtn.addEventListener("click" , reset);
